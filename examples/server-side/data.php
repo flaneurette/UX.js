b299d9bec0773f768cbdd1b69670bee79f147262
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2024-05-08 12:04:28 +0200

    Add files via upload

---
+++ examples/server-side/data.php
@@ -0,0 +1,17 @@
+{"blogs":[
+   {
+      "blogname":"I like Bananas",
+      "blogtext":"Bananas are healthy",
+      "link":"#"
+   },
+   {
+      "blogname":"I love Oranges",
+      "blogtext":"Oranges are healthy",
+      "link":"#"
+   },
+   {
+      "blogname":"I do not like Vegetables",
+      "blogtext":"Vegetables are healthy",
+      "link":"#"
+   }
+]}
