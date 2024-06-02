e0987e66b690956905354a4e0abc78a586656437
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2024-06-02 14:54:53 +0200

    Update Magic.js

--- assets/Magic.js
+++ assets/Magic.js
@@ -20,7 +20,7 @@ class Magic {
       let data = list.data
       let method = list.methods;
       let events = list.events;
-      if(data) { 
+      if(data) {
         for(const [key, value] of Object.entries(data)) {
           if(Array.isArray(value)) {
             this.nodes('bindLoop', key, value);
@@ -28,7 +28,7 @@ class Magic {
             this.nodes('bindOn', data, method, key, value);
           } else {
             this.nodes('bindShow', key, value);
-			this.nodes('bindHide', key, value);
+            this.nodes('bindHide', key, value);
             this.nodes('replaceNodeValue', key, value);
             this.nodes('bindAttributesNode', key, value);
             this.nodes('bindLogic', key, value);
@@ -37,30 +37,30 @@ class Magic {
             this.nodes('bindOn', key, value, data, method);
             this.nodes('bindFlex', key, value, data, method);
             this.nodes('bindActive', key, value);
-			this.nodes('bindMenu', key, value);
+            this.nodes('bindMenu', key, value);
             if(key == 'cleartype' && value == true) this.nodes('clearType', data, key, value);
           }
         }
       }
     }
   }
-  
-  nodes(method, find, value, data=null, methods=null) {
+
+  nodes(method, find, value, data = null, methods = null) {
     var docElements = this.nodeParentList();
     for(var i = 0; i < docElements.length; i++) {
       if(method == 'bindShow') this.bindShow(docElements[i], find, value);
-	  if(method == 'bindHide') this.bindHide(docElements[i], find, value);
+      if(method == 'bindHide') this.bindHide(docElements[i], find, value);
       if(method == 'createForm') this.createForm(docElements[i], find, value);
       if(method == 'bindCurtains') this.bindCurtains(docElements[i], find, value);
       if(method == 'bindLoop') this.loop(docElements[i], find, value);
       if(method == 'bindFunctions') this.bindFunctions(docElements[i], find, value);
-      if(method == 'bindLogic') this.bindIf(docElements[i], find, value); 
+      if(method == 'bindLogic') this.bindIf(docElements[i], find, value);
       if(method == 'bindOn') this.bindOn(docElements[i], data, methods, find, value);
       if(method == 'bindAttributesNode') this.bindClass(docElements[i], find, value);
       if(method == 'clearType') this.clearType(docElements[i], find, value);
       if(method == 'bindFlex') this.bindFlex(docElements[i], find, value);
       if(method == 'bindActive') this.bindActive(docElements[i], find, value);
-	  if(method == 'bindMenu') this.bindMenu(docElements[i], find, value);
+      if(method == 'bindMenu') this.bindMenu(docElements[i], find, value);
       var docChildren = this.nodeChildren(docElements[i]);
       for(var j = 0; j < docChildren.length; j++) {
         if(method == 'replaceNodeValue') {
@@ -107,37 +107,57 @@ class Magic {
       return att;
     }
   }
-  
+
   getAttCheck(node, part) {
-    let check = (node.getAttribute('magic:'+part) !== null 
-    || node.getAttribute('m:'+part) !== null 
-    || node.getAttribute(':'+part) !== null) ? true : false;
+    let check = (node.getAttribute('magic:' + part) !== null ||
+      node.getAttribute('m:' + part) !== null ||
+      node.getAttribute(':' + part) !== null) ? true : false;
     return check;
   }
 
-  dom(id,method,value=null) {
-    if(id !== null) {		
-    switch(method) {
-        case 'id': return document.getElementById(id); break;
-        case 'get': return document.getElementById(id).value; break;
-        case 'set': document.getElementById(id).value = value; break;
-        case 'none': document.getElementById(id).hidden = true; break;
-        case 'block': document.getElementById(id).hidden = false; break;
-        case 'sethtml': document.getElementById(id).innerHTML = value; break;
-        case 'gethtml': return document.getElementById(id).innerHTML; break;
-        case 'display': document.getElementById(id).style.display = value; break;
-        case 'parent': return document.getElementById(id).parentNode; break;
-        case 'children': return document.getElementById(id).children; break;
-	   }
-     }
+  dom(id, method, value = null) {
+    if(id !== null) {
+      switch(method) {
+        case 'id':
+          return document.getElementById(id);
+          break;
+        case 'get':
+          return document.getElementById(id).value;
+          break;
+        case 'set':
+          document.getElementById(id).value = value;
+          break;
+        case 'none':
+          document.getElementById(id).hidden = true;
+          break;
+        case 'block':
+          document.getElementById(id).hidden = false;
+          break;
+        case 'sethtml':
+          document.getElementById(id).innerHTML = value;
+          break;
+        case 'gethtml':
+          return document.getElementById(id).innerHTML;
+          break;
+        case 'display':
+          document.getElementById(id).style.display = value;
+          break;
+        case 'parent':
+          return document.getElementById(id).parentNode;
+          break;
+        case 'children':
+          return document.getElementById(id).children;
+          break;
+      }
+    }
   }
-  
+
   cloneNodes(list, id) {
     if(id === null) {
       return false;
     } else {
-      const parentItem = this.dom(id,'parent');
-      let docItem = this.dom(id,'id');
+      const parentItem = this.dom(id, 'parent');
+      let docItem = this.dom(id, 'id');
       let docClone = docItem.cloneNode(true);
       for(let i = 0; i < list.length; i++) {
         parentItem.appendChild(docClone);
@@ -162,11 +182,11 @@ class Magic {
   drawCurtains() {
     var docElements = Magic.docElements;
     for(var i = 0; i < docElements.length; i++) {
-      if(docElements[i].getAttribute('magic:curtain') !== null 
-        || docElements[i].getAttribute('m:curtain') !== null 
-        || docElements[i].getAttribute(':curtain') !== null) {
+      if(docElements[i].getAttribute('magic:curtain') !== null ||
+        docElements[i].getAttribute('m:curtain') !== null ||
+        docElements[i].getAttribute(':curtain') !== null) {
         docElements[i].hidden = false;
-      } 
+      }
     }
   }
 
@@ -191,79 +211,78 @@ class Magic {
   }
 
   clearType(node, find, value) {
-	let span = /(::s)\s([a-zA-Z][0-9]\s)/gm
-	let div = /(::d)\s([a-zA-Z][0-9]\s)/gm
-	node.innerHTML = node.innerHTML.replaceAll('::d',"<div>");
-	node.innerHTML = node.innerHTML.replaceAll('d::',"</div>");
-	node.innerHTML = node.innerHTML.replaceAll('::s',"<span>");
-	node.innerHTML = node.innerHTML.replaceAll('s::',"</span>");
+    let span = /(::s)\s([a-zA-Z][0-9]\s)/gm
+    let div = /(::d)\s([a-zA-Z][0-9]\s)/gm
+    node.innerHTML = node.innerHTML.replaceAll('::d', "<div>");
+    node.innerHTML = node.innerHTML.replaceAll('d::', "</div>");
+    node.innerHTML = node.innerHTML.replaceAll('::s', "<span>");
+    node.innerHTML = node.innerHTML.replaceAll('s::', "</span>");
   }
- 
+
   bindShow(node) {
     let att = this.getAtt(node, 'hidden');
     if(att !== null) {
       node.hidden = false;
-	}
+    }
   }
-  
+
   bindHide(node) {
     let att = this.getAtt(node, 'hidden');
     if(att !== null && att == 'true') {
       node.hidden = true;
-	}
+    }
   }
-  
+
   bindActive(node, find, value) {
-      let att = this.getAtt(node, 'active');
-      let active = window.location.href;
-      if(att !== null) {
-      if(att.indexOf(':') != -1 ) { 
-          let pieces = att.split(':');
-		if(active.match(pieces[0])) {
-           node.className = pieces[1].toString();
+    let att = this.getAtt(node, 'active');
+    let active = window.location.href;
+    if(att !== null) {
+      if(att.indexOf(':') != -1) {
+        let pieces = att.split(':');
+        if(active.match(pieces[0])) {
+          node.className = pieces[1].toString();
         }
       }
     }
   }
-  
+
   bindFlex(node, find, value) {
     let att = this.getAtt(node, 'flex');
     if(att !== null) {
-    if(att.indexOf(':') != -1 ) { 
-    let flexbox = att.split(':');
-    let flex = 'display:flex;';
-    let flexdir = 'flex-direction:' + flexbox[1] + ';';
-      if(flexbox[0] == 'true' || flexbox[0] == '1' || flexbox[0] == 'start' || flexbox[0] == 'left') node.setAttribute("style",flex + flexdir);
-      if(flexbox[0] == 'end' || flexbox[0] == 'right') node.setAttribute("style", flex + 'justify-content: flex-end;' + flexdir);
-      if(flexbox[0] == 'center') node.setAttribute("style",flex + 'justify-content: center;'+ flexdir);
-      if(flexbox[0] == 'bottom') node.setAttribute("style",flex + 'align-items: baseline;'+ flexdir);
+      if(att.indexOf(':') != -1) {
+        let flexbox = att.split(':');
+        let flex = 'display:flex;';
+        let flexdir = 'flex-direction:' + flexbox[1] + ';';
+        if(flexbox[0] == 'true' || flexbox[0] == '1' || flexbox[0] == 'start' || flexbox[0] == 'left') node.setAttribute("style", flex + flexdir);
+        if(flexbox[0] == 'end' || flexbox[0] == 'right') node.setAttribute("style", flex + 'justify-content: flex-end;' + flexdir);
+        if(flexbox[0] == 'center') node.setAttribute("style", flex + 'justify-content: center;' + flexdir);
+        if(flexbox[0] == 'bottom') node.setAttribute("style", flex + 'align-items: baseline;' + flexdir);
+      }
     }
-   }
   }
- 
+
   bindMenu(node, find, value) {
     let att = this.getAtt(node, 'menu');
-    if(att !== null && att.indexOf(':') !==-1) {
+    if(att !== null && att.indexOf(':') !== -1) {
       let pairs = att.split(':');
-	  
-	  if(pairs[1] == 'in') {
-	    let list = document.getElementById(pairs[0]).children;
-	    for(let i=0; i< list.length; i++) {
-		  list[i].setAttribute(':menu',att);
-	    }
-	    document.getElementById(pairs[0]).hidden = true;
+      if(pairs[1] == 'in') {
+        let list = document.getElementById(pairs[0]).children;
+        for(let i = 0; i < list.length; i++) {
+          list[i].setAttribute(':menu', att);
+        }
+        document.getElementById(pairs[0]).hidden = true;
         node.addEventListener('mouseover', function() {
-        document.getElementById(pairs[0]).hidden = false;
-      });
-	  }
-	  if(pairs[1] == 'out') {
-         node.addEventListener('mouseout', function() {
-         document.getElementById(pairs[0]).hidden = true;
-      });
-	  }
+          document.getElementById(pairs[0]).hidden = false;
+        });
+      }
+      if(pairs[1] == 'out') {
+        node.addEventListener('mouseout', function() {
+          document.getElementById(pairs[0]).hidden = true;
+        });
+      }
     }
   }
-  
+
   bindFunctions(node, find, value) {
     let att = this.getAtt(node, 'click');
     var docElements = Magic.docElements;
@@ -274,20 +293,20 @@ class Magic {
             if(docElements[i].getAttribute(':id') !== null) {
               let attribute = docElements[i].getAttribute(':id');
               if(att = 'count++') {
-              docElements[i].innerText = (Number(docElements[i].innerText) + 1);
-		      }
+                docElements[i].innerText = (Number(docElements[i].innerText) + 1);
+              }
             }
           }
         });
       }
     }
   }
-  
+
   bindMethods(node, data, methods, find, value) {
     let process = new Function(methods);
     process.apply();
   }
-  
+
   bindOn(node, data, methods, find, value) {
     let att = this.getAtt(node, 'click');
     if(this.getAttCheck(node, 'click') == true) {
@@ -296,28 +315,28 @@ class Magic {
           let statics = document.body.innerHTML;
           let findMethod = node.getAttribute(':click');
           if(findMethod !== null) {
-	        if(methods && Object(methods)) {
-                 for(let key in methods) {
-                 let funcs = methods[key];
-                 let pairs = funcs.toString();
-                 let fp = pairs.split("\n");
-                   for(let i=0;i<fp.length;i++) {
-                   // operators
-                    if(fp[i].indexOf('this.') !== -1 && fp[i].indexOf('=') !== -1) {
-                      let ps = fp[i].split('=');
-                      if(ps[1].indexOf('.') !== -1) { 
+            if(methods && Object(methods)) {
+              for(let key in methods) {
+                let funcs = methods[key];
+                let pairs = funcs.toString();
+                let fp = pairs.split("\n");
+                for(let i = 0; i < fp.length; i++) {
+                  // operators
+                  if(fp[i].indexOf('this.') !== -1 && fp[i].indexOf('=') !== -1) {
+                    let ps = fp[i].split('=');
+                    if(ps[1].indexOf('.') !== -1) {
                       // expressions
-                      } else {
-                        // text processing
-                        ps[0] = ps[0].toString().replace(/^\s+|\t+/gm,'');
-                        ps[1] = ps[1].toString().replace(/,|'|"|/gm,'');
-                        let op = ps[0].split('.');
-                        const regex = new RegExp("{{\\s*" + op[1] + "[0-9]*\\s*}}", "gmi");
-                        node.innerHTML = node.innerHTML.replace(regex,ps[1]);         
-                      }
+                    } else {
+                      // text processing
+                      ps[0] = ps[0].toString().replace(/^\s+|\t+/gm, '');
+                      ps[1] = ps[1].toString().replace(/,|'|"|/gm, '');
+                      let op = ps[0].split('.');
+                      const regex = new RegExp("{{\\s*" + op[1] + "[0-9]*\\s*}}", "gmi");
+                      node.innerHTML = node.innerHTML.replace(regex, ps[1]);
                     }
-                   }
-                 // method functions
+                  }
+                }
+                // method functions
                 funcs.apply();
               }
             }
@@ -341,7 +360,9 @@ class Magic {
         }
       } else if(att.search("/\s/")) {
         // operators
-        let toEval = null, key = null, opp = '';
+        let toEval = null,
+          key = null,
+          opp = '';
         let pieces = att.split("\s");
         node.hidden = true;
         for(let i = 0; i < pieces.length; i++) {
@@ -355,7 +376,7 @@ class Magic {
   }
 
   loop(node, find, values) {
-	  
+
     let att = this.getAtt(node, 'loop');
     let attclick = this.getAtt(node, 'click');
     let zebra = this.getAtt(node, 'zebra');
@@ -372,22 +393,22 @@ class Magic {
           if(c.innerHTML) {
             c.innerHTML = c.innerHTML.replace("{{" + k[j] + "}}", v[j]);
             if(c.innerHTML.indexOf("{{" + k[j] + "}}") != -1) {
-               c.innerHTML = c.innerHTML.replace("{{" + k[j] + "}}", v[j]);
-			}
+              c.innerHTML = c.innerHTML.replace("{{" + k[j] + "}}", v[j]);
+            }
           }
-            c.setAttribute('id',find+i);
+          c.setAttribute('id', find + i);
         }
         node.append(c);
         c = c.cloneNode(true);
         if(zebra !== null && node.children[i]) {
-            let mod = 2;
-            let className = zebra;
-            if(zebra.indexOf(':') != -1) { 
-               let parts = zebra.split(':');
-               className = parts[0];
-               mod = parts[1];
-            }
-            if(i % mod !== 0) node.children[i].className = className;
+          let mod = 2;
+          let className = zebra;
+          if(zebra.indexOf(':') != -1) {
+            let parts = zebra.split(':');
+            className = parts[0];
+            mod = parts[1];
+          }
+          if(i % mod !== 0) node.children[i].className = className;
         }
       }
     }
@@ -400,19 +421,19 @@ class Magic {
     req.setRequestHeader('Access-Control-Allow-Origin', Magic.allowOrigin);
     req.setRequestHeader("Content-Type", Magic.contentType);
     if(method == 'callback') {
-    req.onreadystatechange = function() {
-      if(req.readyState == 4 && req.status == 200) {
-        callback(req.responseText);
+      req.onreadystatechange = function() {
+        if(req.readyState == 4 && req.status == 200) {
+          callback(req.responseText);
+        }
       }
-    }
-    req.send();  
+      req.send();
     } else if(method == 'get') {
-    req.onreadystatechange = function() {
-      if(req.readyState == 4 && req.status == 200) {
-        return JSON.parse(req.responseText);
+      req.onreadystatechange = function() {
+        if(req.readyState == 4 && req.status == 200) {
+          return JSON.parse(req.responseText);
+        }
       }
-    }
-    req.send();
+      req.send();
     }
   }
 
@@ -435,7 +456,7 @@ class Magic {
     let att = this.getAtt(node, 'form');
     if(att !== null) {
       let parents = document.createElement('div');
-	  node.appendChild(parents);
+      node.appendChild(parents);
       let options = document.createElement('form');
       for(let key in values) {
         let arr = values[key];
@@ -462,11 +483,11 @@ class Magic {
       parents.appendChild(options);
     }
   }
-  
+
   log(msg) {
     console.log(msg);
   }
-  
+
   msg = {
     initialize: "Magic: Cannot initialize a non-object.",
     enumerate: "Magic: Could not enumerate global object."
