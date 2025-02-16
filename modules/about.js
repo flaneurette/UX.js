00bbfcc8c8f4af436a2cf0e077d8060a6c1af9c9
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2025-02-16 19:30:18 +0100

    Update about.js

--- modules/about.js
+++ modules/about.js
@@ -1,10 +1,8 @@
 export const About = {
 	id: 'About',
-    render: () => `
+    	render: () => `
 	<h1>About Page</h1>
-	<p>
-		This is reactive modular UX.JS.
-	</p>
+	<p>This is reactive modular UX.JS.</p>
 	`,
 	init: () => null,
 };
