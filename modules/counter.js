8bdde28d354c02aaf231d5a74a074c41159525b1
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2025-02-17 14:53:51 +0100

    Kola
    Kola

--- modules/counter.js
+++ modules/counter.js
@@ -1,23 +1,23 @@
 export const Counter = {
-	
+
 	id: 'Counter',
 	
-    init() {
+	init() {
 		this.count = 0;
-    },
+	},
 
-    render() {
+    	render() {
 	return `
 		<div>
 			<h2>Count: <span id="counter">${this.count}</span></h2>
 			<button data-action="increment">Increment</button>
 		</div>
-	`;
-    },
+		`;
+    	},
 
-    increment() {
+    	increment() {
 		let counterElement = document.querySelector('#counter');
 		let count = parseInt(counterElement.innerHTML, 10) || 0;
 		counterElement.innerHTML = count + 1;
-    }
+    	}
 }
