71779ba3c5df22a81ba3ab58e791089abc874270
Flaneurette <30347642+flaneurette@users.noreply.github.com>
2025-02-22 23:39:39 +0100

    Reactive
    Reactive

--- modules/reactive/form.js
+++ modules/reactive/form.js
@@ -10,43 +10,43 @@ export const Form = {
 
 	render() {
 		return `
-        <h1>Form</h1>
-        <form>
-            <fieldset>
-              <legend>Do you agree to our terms?</legend>
-              <input type="radio" name="agreed" id="yes" value="Yes" 
-                ${this.agreed === 'Yes' ? 'checked' : ''} 
-                onChange="reactiveChecked(event.target.value);" />
-              <label for="yes">Yes</label>
-              <br />
-              <input type="radio" name="agreed" id="no" value="No" 
-                ${this.agreed === 'No' ? 'checked' : ''} 
-                onChange="reactiveChecked(event.target.value);" />
-              <label for="no">No</label>
-            </fieldset>
-        </form>
-        <p>
-            <strong>Agreed to:</strong>
-			<span id="agreed">${this.agreed}</span>
-        </p>
+		<h1>Form</h1>
 		<form>
-        <fieldset>
-          <legend>Select items:</legend>
-              <input type="checkbox" name="foods" id="apples" value="Apples" 
-                ${this.food.includes('Apples') ? 'checked' : ''} 
-                onChange="reactiveFoods(event.target.value,event.target.checked);" />
-              <label for="apples">Apples</label>
-              <input type="checkbox" name="foods" id="oranges" value="Oranges" 
-                ${this.food.includes('Oranges') ? 'checked' : ''} 
-                onChange="reactiveFoods(event.target.value,event.target.checked);" />
-              <label for="oranges">Oranges</label>
-        </fieldset>
-      </form>
-	    <p>
-            <strong>Foods:</strong>
+			<fieldset>
+				<legend>Do you agree to our terms?</legend>
+				<input type="radio" name="agreed" id="yes" value="Yes" 
+				${this.agreed === 'Yes' ? 'checked' : ''} 
+				onChange="reactiveChecked(event.target.value);" />
+				<label for="yes">Yes</label>
+				<br />
+				<input type="radio" name="agreed" id="no" value="No" 
+				${this.agreed === 'No' ? 'checked' : ''} 
+				onChange="reactiveChecked(event.target.value);" />
+				<label for="no">No</label>
+			</fieldset>
+		</form>
+		<p>
+			<strong>Agreed to:</strong>
+			<span id="agreed">${this.agreed}</span>
+		</p>
+		<form>
+		<fieldset>
+			<legend>Select items:</legend>
+				<input type="checkbox" name="foods" id="apples" value="Apples" 
+				${this.food.includes('Apples') ? 'checked' : ''} 
+				onChange="reactiveFoods(event.target.value,event.target.checked);" />
+				<label for="apples">Apples</label>
+				<input type="checkbox" name="foods" id="oranges" value="Oranges" 
+				${this.food.includes('Oranges') ? 'checked' : ''} 
+				onChange="reactiveFoods(event.target.value,event.target.checked);" />
+				<label for="oranges">Oranges</label>
+		</fieldset>
+		</form>
+		<p>
+			<strong>Foods:</strong>
 			<span id="foods">${app.state.foods}</span>
-        </p>
-	  `;
+		</p>
+		`;
 	},
 }
 
